6908b10cc3aa337de395a104d83e2b7c81485522
Elizabeth Cristina <lizzy.cristina@gmail.com>
2024-01-31 11:11:21 -0600

    Initial controllers: auth, home, profile

---
+++ controllers/profile.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+
+module.exports = {
+    getProfile: async (req, res) => {
+        try {
+            const user = await User.find({ user: req.user.id });
+            res.render('profile.ejs', { user:req.user });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+}
